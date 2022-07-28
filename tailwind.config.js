122d8793d59eb7fbede99b813decb524ed9bbf33
Laraib Khan <visitlaraib@gmail.com>
2022-07-28 11:19:16 +0530

    paheli baar

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [],
+    theme: {
+        colors: {
+            green_as_want: '#0599a9',
+        }
+        extend: {
+            backgroundImage: {
+                'bg-sofa': "url('/img/sofa.png')"
+            }
+        },
+    },
+    plugins: [],
+}
